76f9072bda0d78b9dbe662eea2fa3416520edba1
KeenanLukeOM <76129769+KeenanLukeOM@users.noreply.github.com>
2024-04-17 23:34:15 -0700

    Solution 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let joinedString = string
 
-};
+    if (num === 0) {
+        return ""
+    } else if (num < 0) {
+        return "ERROR"
+    } else {
+    for (i = 1; i < num; i++) {
+        joinedString = joinedString + string
+    }
+    return joinedString }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
