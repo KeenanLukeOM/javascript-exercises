6612ab41cfd74be4633fa3d63c695a337a5212a9
KeenanLukeOM <76129769+KeenanLukeOM@users.noreply.github.com>
2024-04-18 02:43:14 -0700

    05 Solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
 
+    let sum = 0
+
+    if (firstNumber < 0 
+        || lastNumber < 0 
+        || typeof firstNumber !== "number" 
+        || typeof lastNumber !== "number") {
+        return "ERROR"
+    } else if(firstNumber < lastNumber) {
+        for (i = firstNumber; i <= lastNumber; i++ ) {
+            sum = sum + i
+        }
+    } else {
+        for (i = firstNumber; i >= lastNumber; i-- ) {
+            sum = sum + i
+        }
+    }
+    return sum
 };
 
 // Do not edit below this line
