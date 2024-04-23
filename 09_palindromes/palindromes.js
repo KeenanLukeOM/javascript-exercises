f78c68725389191984ed8fd738d3fbb044cb2266
KeenanLukeOM <76129769+KeenanLukeOM@users.noreply.github.com>
2024-04-23 00:59:37 -0700

    09 Solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    let alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+    let fix = string
+                    .toLowerCase()
+                    .split('')
+                    .filter((char) => alphanumerical.includes(char))
+                    .join('')
+
+    let reverseFixed = fix
+                        .split('')
+                        .reverse()
+                        .join('')
+    
+
+    if (fix === reverseFixed) {
+        return true
+    }
+    return false
 };
-
 // Do not edit below this line
 module.exports = palindromes;
