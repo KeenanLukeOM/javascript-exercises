3ae9c4e2639cfc243fdf65d83a030958a02b4c47
KeenanLukeOM <76129769+KeenanLukeOM@users.noreply.github.com>
2024-04-23 15:51:52 -0700

    10 Solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(seq) {
 
+    if (seq == 0) {
+        return 0
+    } else if (seq < 0) {
+        return 'OOPS'
+    }
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= seq; i++) {
+        let current = firstPrev + secondPrev
+        secondPrev = firstPrev
+        firstPrev = current
+    }
+
+    return firstPrev
 };
 
 // Do not edit below this line
